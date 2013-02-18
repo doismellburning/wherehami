816e21d8d94d9f632b111dc753bc18c383d684a5
Kristian Glass <git@doismellburning.co.uk>
2013-02-18 19:52:17 +0000

    Context processors to get template id/keys from env

--- wherehami/wherehami.py
+++ wherehami/wherehami.py
@@ -2,6 +2,14 @@ import flask
 
 app = flask.Flask(__name__)
 
+@app.context_processor
+def inject_google_analytics_id():
+    return {'GOOGLE_ANALYTICS_ID': os.getenv('GOOGLE_ANALYTICS_ID')}
+
+@app.context_processor
+def inject_google_maps_api_key():
+    return {'GOOGLE_MAPS_API_KEY': os.getenv('GOOGLE_MAPS_API_KEY')}
+
 @app.route('/')
 def index():
     return flask.render_template('wherehami.html')
